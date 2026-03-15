0623cc8a2969e0f30ccb02ca24130738e6ae9b52
arinsarker07-oss <arinsarker07@gmail.com>
2026-03-15 18:03:50 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Countries from './player'
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+        <Countries></Countries>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/player.jsx
@@ -0,0 +1,10 @@
+
+const Countries = () => {
+    return (
+        <div>
+          <h2 style={{textAlign : 'left', padding:'20px'}}>In the countries</h2>
+        </div>
+    );
+};
+
+export default Countries;
