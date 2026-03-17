10c555d0e3ee942fe68ff50f403bc61a3ccef171
arinsarker07-oss <arinsarker07@gmail.com>
2026-03-17 11:08:02 +0600

    peoject done

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
-
-const Country = () => {
+import React, { useState } from 'react';
+import "./country.css"
+const Country = ({country ,handlevisited, HandlevisitedFlag}) => {
+    // console.log(country);
+    const [visited, setVisited]=useState(false)
+    const handebtn=()=>{
+       setVisited(!visited)
+       handlevisited(country);
+    }
     return (
-        <div>
-            <h3>Name:</h3>
+        <div className={`country ${visited &&" country-visited"}`}>
+            <img src={country?.flags?.flags?.png} alt="" />
+            <h3>Name: {country.name.common}</h3>
+            <p >population :{country.population.population}</p>
+            <p>area: {country.area.area} {country.area.area>300000 ? "Big country" : "small country"}</p>
+            <button onClick={handebtn}>{visited ? "visited" : "not visited"}</button>
+            <button style={{marginLeft: "10px"}} onClick={()=>{HandlevisitedFlag(country?.flags?.flags?.png)}}>visited flag</button>
         </div>
     );
 };
--- src/countries.jsx
+++ src/countries.jsx
@@ -1,12 +1,47 @@
-import { use } from "react";
-
+import { use, useState } from "react";
+import Country from './Country'
+import "./countries.css"
 const Countries = ({countriesPromise}) => {
     const countriesData= use(countriesPromise);
     const countries=countriesData.countries;
-    console.log(countries);
+    const [VisitedCountry,setVisitedCountry]=useState([]);
+    const [visitedFlag, setVisitedFlag]=useState([])
+    const handlevisited=(country)=>{
+      const nervisitedcountry=[...VisitedCountry ,country];
+      setVisitedCountry(nervisitedcountry)
+    }
+    const HandlevisitedFlag=(flag)=>{
+      const newvisit=[...visitedFlag, flag]
+      setVisitedFlag(newvisit)
+      console.log(flag);
+      
+    }
     return (
-        <div>
+        <div style={{textAlign : 'left'}}>
           <h2 style={{textAlign : 'left', padding:'20px'}}>In the countries: {countries.length}</h2>
+          <h3>Total country visited : {VisitedCountry.length}</h3>
+          <ol>
+            {
+              VisitedCountry.map(country=> <li
+                key={country.cca3.cca3}
+              >{country.name.common}</li>)
+            }
+          </ol>
+          <div className="visited-flag">
+            {
+              visitedFlag.map((flag ,index)=> <img key={index} src={flag}></img>)
+            }
+          </div>
+          <div className="countries">
+            {
+            countries.map(country=><Country 
+              key={country.cca3.cca3}
+              country={country}
+              handlevisited={handlevisited}
+              HandlevisitedFlag={HandlevisitedFlag}
+              ></Country>)
+          }
+          </div>
         </div>
     );
 };
